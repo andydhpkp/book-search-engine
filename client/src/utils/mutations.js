dcf6ec419a4fd80704ce51cf825dd72085e9846d
Andrew Durham <andrewdurham1094@gmail.com>
2021-12-18 13:37:26 -0700

    finished pages

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,7 +1,64 @@
-import gql from 'graphql-tag'
+import gql from '@apollo/client'
 
 export const LOGIN_USER = gql`
     mutation loginUser($email: String!, $password: String!) {
-        login(email: $email, password)
+        login(email: $email, password) {
+            token
+            user {
+                _id
+                username
+            }
+        }
+    }
+`
+
+export const ADD_USER = gql`
+    mutation addUser($username: String!, $email: String!, $password: String!) {
+        addUser(username: $username, email: $email, password: $password) {
+            token
+            user {
+                _id
+                username
+            }
+        }
+    }
+`
+
+export const SAVE_BOOK = gql`
+    mutation saveBook($input: savedBook!) {
+        saveBook(input: $input) {
+            _id
+            username
+            email
+            bookCount
+            savedBooks {
+                _id
+                authors
+                description
+                bookId
+                image
+                link
+                title
+            }
+        }
+    }
+`
+
+export const REMOVE_BOOK = gql`
+    mutation removeBook($bookId: String!) {
+        removeBook(bookId: $bookId) {
+            _id
+            username
+            email
+            bookCount
+            savedBooks {
+                authors
+                description
+                bookId
+                image
+                link
+                title
+            }
+        }
     }
 `
--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,5 +1,18 @@
-import gql from 'graphql-tag'
+import { gql } from "@apollo/client"
 
 export const GET_ME = gql`
-    query me()
+{
+    me {
+        _id
+        username
+        email
+        bookCount
+        savedBooks {
+            bookId
+            title
+            authors
+            image
+        }
+    }
+}
 `
